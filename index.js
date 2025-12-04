e0efaaf1c5238812a85c0530b87ddc1c60b8374b
aurelie_dumotier <aurelie.dumotier@gmail.com>
2025-12-04 10:19:56 +0100

    initialization API druidolib

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import {praticiens} from './praticiens.js'; 
+// const express = require('express')
+
+const app = express()
+
+app.use(cors('*'))
+
+app.get('/', (req, res) => {
+    console.log('tu est sur la route / ')
+    res.send('tu est sur la route / ')
+})
+
+// Get http://localhost:4242/praticiens
+app.get('/praticiens', (req, res) => {
+    console.log('tu est sur la route /praticiens ')
+    res.json(praticiens)
+})
+
+app.listen(4242, () => {
+    console.log('http://localhost:4242/')
+})
---
+++ praticiens.js
@@ -0,0 +1,24 @@
+export const praticiens = [
+{
+  id: 1,
+  nom: "Chênefort Belmir",
+  age: "302 ans",
+  specialite: "Phytomagie ancestrale et divination approximative",
+  presentation: "Druide depuis plus de 150 lunes. Parle aux plantes, prétend qu'elles répondent, et commence souvent ses consultations par 'As-tu remercié ton arbre préféré aujourd'hui ?'",
+  tarif: "40€ la consultation (ou 3 glands + 1 branche de gui)",
+  adresse: "Clairière du Vieux Chêne, troisième tronc à droite. 12 min à pied si les esprits sont de bonne humeur",
+  imagePraticien: "https://i.pinimg.com/736x/f8/8f/5c/f88f5c46a3c0bee8877107934067aeaf.jpg",
+  imageLieu: "https://i.pinimg.com/736x/47/98/92/479892d5c43a00bd77ea345ec4adaa13.jpg"
+},
+{
+  id: 2,
+  nom: "Ysoria la Clairvoyante du Bois-Brume",
+  age: "43 ans",
+  specialite: "Voyance runique et interprétation des murmures astrals",
+  description: "Maîtresse des visions étranges, Ysoria contemple les volutes d'encens jusqu’à ce que les esprits acceptent enfin de lui répondre. Elle parle souvent en devinettes, parfois même sans raison, mais ses prophéties – qu'elles soient terrifiantes ou absurdes – finissent toujours par se réaliser d’une manière ou d’une autre.",
+  tarif: "3 pièces d’argent, ou une plume de griffon impeccablement propre.",
+  adresse: "Marchez vers le nord jusqu’à ce que les arbres semblent vous observer, tournez à gauche au rocher qui ressemble vaguement à un hérisson géant, puis avancez jusqu'à sentir une odeur de sauge brûlée. Sa hutte apparaît lorsque vous clignez des yeux trois fois.",
+  imagePraticien: "https://i.pinimg.com/736x/a1/36/8e/a1368ec5af24b2240f9362e2bb18017b.jpg",
+  imageLieu: "https://i.pinimg.com/736x/83/01/ef/8301eff5fdeac85c0fd1e0e1c6ca6ff8.jpg"
+}
+];
